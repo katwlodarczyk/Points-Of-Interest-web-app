f974b278323debb83451a30f50cae7b2ed1fadd6
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2021-03-26 14:47:18 +0000

    added search functionality and style

--- public/index.js
+++ public/index.js
@@ -1,3 +1,112 @@
+// SEARCH 
+async function poiSearch(type, field) {
+    
+  // Send a request to our remote URL
+  const response = await  fetch(`/poi/${type}/${field}`);
+
+  // Parse the JSON.
+  const result = await response.json();
+  // Loop through the array of JSON objects and add the results to a <div>
+  let html = "";
+  document.getElementById('results').innerHTML = "";
+  let resultsdivsstart =
+    `
+    <h2 class="sm:px-6 lg:px-8 py-4 text-2xl text-brand-navy font-extrabold">Your results:</h2>
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+    `
+
+  let resultsdivsend =
+    `       </tbody
+          </table>
+        </div>
+      </div>
+    `
+  let tableHeader = 
+    `
+    <thead class="bg-brand-light-gray">
+      <tr>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Name
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Type
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Country
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Region
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Lon
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Lat
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Description
+        </th>
+        <th scope="col" class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          Recommendations
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+  `;
+
+  let noResultsNotification = `<h2 class="sm:px-6 lg:px-8 py-4 text-2xl text-brand-navy font-extrabold">Your results:</h2><p class="text-brand-navy text-xl font-normal sm:px-6 lg:px-8">Oops... Sorry, no matching results. Plase check your spelling or search for something else!</p>`
+
+  if (result.length === 0){
+      document.getElementById('results').innerHTML = noResultsNotification + html;
+  } else {
+      result.forEach ( result => {
+        html += `
+  
+            <tr class="bg-white">
+              <td class="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              ${result.name}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.type}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.country}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.region}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.lon}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.lat}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.description}
+              </td>
+              <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+              ${result.recommendations}
+              
+              </td>
+            </tr>
+          `
+      });
+      document.getElementById('results').innerHTML = resultsdivsstart + tableHeader + html + resultsdivsend;
+  }
+}
+
+
+// Make the AJAX run when we click a search button
+document.getElementById('seachButton').addEventListener('click', ()=> {
+  const type = document.getElementById('poiType').value;
+  const field = document.getElementById('poiField').value;
+  poiSearch(type, field);
+});
+
+
+
 // AUTOMATIC DATE UPDATE FOR FOOTER
 
 const footerYear = document.getElementById("year");
@@ -23,3 +132,4 @@ function hamburgerMenu() {
       x.classList.remove("responsive");
     }
   }
+
